c97dbfa41e4708383cb772441a60d0a9b82ff62b
Marc Silanus <msilanus@gmail.com>
2017-11-26 00:37:58 +0100

    First commit

---
+++ zigbeeConfigurator/zigbeeConfigurator/zigbeeConfigurator.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace zigbeeConfigurator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.grbConfigurer = new System.Windows.Forms.GroupBox();
+            this.btnConfigurer = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbDeviceName = new System.Windows.Forms.TextBox();
+            this.rdbRouter = new System.Windows.Forms.RadioButton();
+            this.rdbCoordinator = new System.Windows.Forms.RadioButton();
+            this.txtPANID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grbTerminal = new System.Windows.Forms.GroupBox();
+            this.txbTerm = new System.Windows.Forms.TextBox();
+            this.grbTransmettre = new System.Windows.Forms.GroupBox();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnEnvoyer = new System.Windows.Forms.Button();
+            this.txbToSend = new System.Windows.Forms.TextBox();
+            this.grbConfigurer.SuspendLayout();
+            this.grbTerminal.SuspendLayout();
+            this.grbTransmettre.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(123, 12);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(140, 24);
+            this.cmbPorts.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choisir un port";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(283, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 30);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Tag = "0";
+            this.btnConnect.Text = "Connexion";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.ReadTimeout = 1000;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // grbConfigurer
+            // 
+            this.grbConfigurer.Controls.Add(this.btnConfigurer);
+            this.grbConfigurer.Controls.Add(this.label3);
+            this.grbConfigurer.Controls.Add(this.txbDeviceName);
+            this.grbConfigurer.Controls.Add(this.rdbRouter);
+            this.grbConfigurer.Controls.Add(this.rdbCoordinator);
+            this.grbConfigurer.Controls.Add(this.txtPANID);
+            this.grbConfigurer.Controls.Add(this.label2);
+            this.grbConfigurer.Enabled = false;
+            this.grbConfigurer.Location = new System.Drawing.Point(12, 57);
+            this.grbConfigurer.Name = "grbConfigurer";
+            this.grbConfigurer.Size = new System.Drawing.Size(391, 249);
+            this.grbConfigurer.TabIndex = 3;
+            this.grbConfigurer.TabStop = false;
+            this.grbConfigurer.Text = "configuration du module Zigbee";
+            // 
+            // btnConfigurer
+            // 
+            this.btnConfigurer.Location = new System.Drawing.Point(116, 199);
+            this.btnConfigurer.Name = "btnConfigurer";
+            this.btnConfigurer.Size = new System.Drawing.Size(148, 33);
+            this.btnConfigurer.TabIndex = 6;
+            this.btnConfigurer.Text = "Configurer";
+            this.btnConfigurer.UseVisualStyleBackColor = true;
+            this.btnConfigurer.Click += new System.EventHandler(this.btnConfigurer_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nom du module";
+            // 
+            // txbDeviceName
+            // 
+            this.txbDeviceName.Location = new System.Drawing.Point(42, 158);
+            this.txbDeviceName.Name = "txbDeviceName";
+            this.txbDeviceName.Size = new System.Drawing.Size(100, 22);
+            this.txbDeviceName.TabIndex = 4;
+            // 
+            // rdbRouter
+            // 
+            this.rdbRouter.AutoSize = true;
+            this.rdbRouter.Checked = true;
+            this.rdbRouter.Location = new System.Drawing.Point(42, 116);
+            this.rdbRouter.Name = "rdbRouter";
+            this.rdbRouter.Size = new System.Drawing.Size(80, 21);
+            this.rdbRouter.TabIndex = 3;
+            this.rdbRouter.TabStop = true;
+            this.rdbRouter.Text = "Routeur";
+            this.rdbRouter.UseVisualStyleBackColor = true;
+            // 
+            // rdbCoordinator
+            // 
+            this.rdbCoordinator.AutoSize = true;
+            this.rdbCoordinator.Location = new System.Drawing.Point(42, 79);
+            this.rdbCoordinator.Name = "rdbCoordinator";
+            this.rdbCoordinator.Size = new System.Drawing.Size(111, 21);
+            this.rdbCoordinator.TabIndex = 2;
+            this.rdbCoordinator.Text = "Coordinateur";
+            this.rdbCoordinator.UseVisualStyleBackColor = true;
+            // 
+            // txtPANID
+            // 
+            this.txtPANID.Location = new System.Drawing.Point(42, 38);
+            this.txtPANID.Name = "txtPANID";
+            this.txtPANID.Size = new System.Drawing.Size(100, 22);
+            this.txtPANID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "PAN ID";
+            // 
+            // grbTerminal
+            // 
+            this.grbTerminal.Controls.Add(this.txbTerm);
+            this.grbTerminal.Location = new System.Drawing.Point(12, 410);
+            this.grbTerminal.Name = "grbTerminal";
+            this.grbTerminal.Size = new System.Drawing.Size(388, 175);
+            this.grbTerminal.TabIndex = 4;
+            this.grbTerminal.TabStop = false;
+            this.grbTerminal.Text = "Terminal";
+            // 
+            // txbTerm
+            // 
+            this.txbTerm.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txbTerm.Font = new System.Drawing.Font("Courier", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTerm.ForeColor = System.Drawing.SystemColors.Info;
+            this.txbTerm.Location = new System.Drawing.Point(6, 24);
+            this.txbTerm.Multiline = true;
+            this.txbTerm.Name = "txbTerm";
+            this.txbTerm.Size = new System.Drawing.Size(376, 145);
+            this.txbTerm.TabIndex = 0;
+            // 
+            // grbTransmettre
+            // 
+            this.grbTransmettre.Controls.Add(this.btnLine);
+            this.grbTransmettre.Controls.Add(this.btnEnvoyer);
+            this.grbTransmettre.Controls.Add(this.txbToSend);
+            this.grbTransmettre.Enabled = false;
+            this.grbTransmettre.Location = new System.Drawing.Point(12, 312);
+            this.grbTransmettre.Name = "grbTransmettre";
+            this.grbTransmettre.Size = new System.Drawing.Size(391, 92);
+            this.grbTransmettre.TabIndex = 5;
+            this.grbTransmettre.TabStop = false;
+            this.grbTransmettre.Text = "Transmettre";
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(226, 44);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(160, 33);
+            this.btnLine.TabIndex = 5;
+            this.btnLine.Text = "Nouvelle Ligne";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnEnvoyer
+            // 
+            this.btnEnvoyer.Location = new System.Drawing.Point(4, 44);
+            this.btnEnvoyer.Name = "btnEnvoyer";
+            this.btnEnvoyer.Size = new System.Drawing.Size(159, 33);
+            this.btnEnvoyer.TabIndex = 4;
+            this.btnEnvoyer.Text = "Envoyer";
+            this.btnEnvoyer.UseVisualStyleBackColor = true;
+            this.btnEnvoyer.Click += new System.EventHandler(this.btnEnvoyer_Click);
+            // 
+            // txbToSend
+            // 
+            this.txbToSend.Location = new System.Drawing.Point(4, 16);
+            this.txbToSend.Name = "txbToSend";
+            this.txbToSend.Size = new System.Drawing.Size(376, 22);
+            this.txbToSend.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 597);
+            this.Controls.Add(this.grbTransmettre);
+            this.Controls.Add(this.grbTerminal);
+            this.Controls.Add(this.grbConfigurer);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbConfigurer.ResumeLayout(false);
+            this.grbConfigurer.PerformLayout();
+            this.grbTerminal.ResumeLayout(false);
+            this.grbTerminal.PerformLayout();
+            this.grbTransmettre.ResumeLayout(false);
+            this.grbTransmettre.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox grbConfigurer;
+        private System.Windows.Forms.Button btnConfigurer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbDeviceName;
+        private System.Windows.Forms.RadioButton rdbRouter;
+        private System.Windows.Forms.RadioButton rdbCoordinator;
+        private System.Windows.Forms.TextBox txtPANID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grbTerminal;
+        private System.Windows.Forms.TextBox txbTerm;
+        private System.Windows.Forms.GroupBox grbTransmettre;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnEnvoyer;
+        private System.Windows.Forms.TextBox txbToSend;
+    }
+}
+
---
+++ zigbeeConfigurator/zigbeeConfigurator/zigbeeConfigurator.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace zigbeeConfigurator
+{
+    public partial class Form1 : Form
+    {
+        string receiveFromSerial;
+        private bool listenMode=true;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (String portname in System.IO.Ports.SerialPort.GetPortNames()) cmbPorts.Items.Add(portname);
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Tag.ToString() == "0")
+            {
+                try
+                {
+                    if (cmbPorts.SelectedItem == null) MessageBox.Show("Choisissez un port !", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else
+                    {
+                        serialPort1.PortName = cmbPorts.SelectedItem.ToString();
+                        serialPort1.Open();
+                        //btnConnect.Enabled = false;
+                        grbConfigurer.Enabled = true;
+                        grbTransmettre.Enabled = true;
+                        btnConnect.Text = "Déconnexion";
+                        btnConnect.Tag = 1;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Impossible d'ouvrir le port " + serialPort1.PortName, "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                serialPort1.Close();
+                txbTerm.Clear();
+                txbDeviceName.Clear();
+                btnConnect.Text = "Connexion";
+                btnConnect.Tag = 0;
+                grbConfigurer.Enabled = false;
+                grbTransmettre.Enabled = false;
+                listenMode = true;
+            }
+        }
+
+        private void btnConfigurer_Click(object sender, EventArgs e)
+        {
+            if (txtPANID.Text == "") MessageBox.Show("Configurez le PAN ID ", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                listenMode = false;
+                serialPort1.Write("+++");
+                if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                {
+                    MessageBox.Show("Impossible d'entrer en mode COMMAND", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    txbTerm.AppendText("+++\r\n" + receiveFromSerial + "\n");
+
+                    serialPort1.Write("ATID" + txtPANID.Text + "\r");
+                    if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                    {
+                        MessageBox.Show("Impossible de configurer PAN ID", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else txbTerm.AppendText("ATID" + txtPANID.Text + "\r\n" + receiveFromSerial + "\n");
+                    if (rdbCoordinator.Checked)
+                    {
+                        serialPort1.Write("ATJV0\r");
+                        if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                        {
+                            MessageBox.Show("Impossible de configurer JV0", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else txbTerm.AppendText("ATJV0\r\n" + receiveFromSerial + "\n");
+
+                        serialPort1.Write("ATDLFFFF\r");
+                        if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                        {
+                            MessageBox.Show("Impossible de configurer le mode Broadcast", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else txbTerm.AppendText("ATDLFFFF\r\n" + receiveFromSerial + "\n");
+
+                        Thread.Sleep(2000);
+                        serialPort1.Write("ATCE1\r");
+                        if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                        {
+                            MessageBox.Show("Impossible de configurer CE1", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else txbTerm.AppendText("ATCE1\r\n" + receiveFromSerial + "\n");
+                        Thread.Sleep(1000);
+                    }
+                    if (rdbRouter.Checked)
+                    {
+                        serialPort1.Write("ATJV1\r");
+                        if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                        {
+                            MessageBox.Show("Impossible de configurer JV1", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else txbTerm.AppendText("ATJV1\r\n" + receiveFromSerial + "\n");
+
+                        serialPort1.Write("ATCE0\r");
+                        if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                        {
+                            MessageBox.Show("Impossible de configurer CE0", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else txbTerm.AppendText("ATCE0\r\n" + receiveFromSerial + "\n");
+                    }
+                    if (txbDeviceName.Text != "") serialPort1.Write("ATNI" + txbDeviceName.Text + "\r");
+                    if ((receiveFromSerial = serialPort1.ReadTo("\r")) != "OK")
+                    {
+                        MessageBox.Show("Impossible de configurer le nom du device", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else txbTerm.AppendText("ATNI" + txbDeviceName.Text + "\r\n" + receiveFromSerial + "\n");
+                }
+
+                serialPort1.Write("ATWR\r");
+                if ((receiveFromSerial = serialPort1.ReadTo("\r")) == "OK")
+                {
+                    MessageBox.Show("Configuration enregistrée !", "Message !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else MessageBox.Show("Impossible d'enregitrer la configuration", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                serialPort1.Write("ATAC\r");
+                Thread.Sleep(1000);
+                serialPort1.Write("ATCN\r");
+                if ((receiveFromSerial = serialPort1.ReadTo("\r")) == "OK")
+                {
+                    MessageBox.Show("Fin du mode COMMAND", "Message !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txbTerm.Clear();
+                    listenMode = true;
+                }
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (listenMode)
+            {
+                receiveFromSerial = serialPort1.ReadExisting();
+                Invoke(new EventHandler(updateTerm));
+            }
+        }
+
+        private void updateTerm(object sender, EventArgs e)
+        {
+            txbTerm.AppendText(receiveFromSerial);
+        }
+
+        private void btnEnvoyer_Click(object sender, EventArgs e)
+        {
+            if (txbToSend.Text != "") serialPort1.Write(txbToSend.Text);
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("\r\n");
+        }
+
+    }
+}
